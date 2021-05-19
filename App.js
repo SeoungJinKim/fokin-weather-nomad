7252014388bf9f78f364b84f3212a7d3e1a8ea85
SeongJin Kim <dge03078@gmail.com>
2021-05-19 20:39:04 +0900

    1.2~1.3 Getting the Location, Asking for Permissions
    Get location info
    Ask permissions to user

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { Alert } from 'react-native'
 import Loading from './Loading'
+import * as Location from 'expo-location'
 
-export default function App() {
-  return <Loading></Loading>
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  }
+  getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync() // 강의에서는 requestPermissionsAsync를 사용했으나 deprecated됨
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync()
+      this.setState({ isLoading: false })
+    } catch (error) {
+      Alert.alert("Can't find you.", 'So sad')
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation()
+  }
+
+  render() {
+    const { isLoading } = this.state
+    return isLoading ? <Loading /> : null
+  }
 }
