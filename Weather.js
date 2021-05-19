3b44fdeabe020061abdf63b5b228ad5f2668193a
SeongJin Kim <dge03078@gmail.com>
2021-05-20 00:30:22 +0900

    2.2 Icons and Styling
    Use MaterialCommunityIcons inside of @expo/vector-icons

--- Weather.js
+++ Weather.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import PropTypes from 'prop-types'
-
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 export default function Weather({ temp }) {
   return (
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons
+          name="weather-lightning-rainy"
+          size={86}
+          color="black"
+        />
+        <Text style={styles.temp}>{temp}°</Text>
+      </View>
+      <View style={styles.halfContainer}></View>
     </View>
   )
 }
@@ -29,6 +37,14 @@ Weather.prototype = {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 42,
+  },
+  halfContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
